d0047ac0f1ed11cac8e2c783426a9a167d55b27a
workworkworkworkwork247365 <149482238+workworkworkworkwork247365@users.noreply.github.com>
2024-10-16 17:40:36 +0800

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -7,9 +7,9 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  token = var.aws_token 
-}
+//provider "aws" {
+//  region = var.aws_region
+//  access_key = var.aws_access_key
+//  secret_key = var.aws_secret_key
+//  token = var.aws_token 
+//}
